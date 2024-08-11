1f2a67f8805e52ce220aeb6191c6fc6dca208e84
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-11 08:39:45 +0300

    modified

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,21 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <WelcomeMessage />
+        <Header />
+        <MainContent />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,8 +35,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   );
 }
 
-export default App
+export default App;
