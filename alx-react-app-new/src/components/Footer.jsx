dcf028a42d7a9b0a176c0f2af429fc7f12fc0b27
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-12 06:55:43 +0300

    modified

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>© 2023 City Lovers</p>
+      <p style={{ color: "lightblue" }}>© 2023 City Lovers</p>
     </footer>
   );
 }
 
-export default Footer
+export default Footer;
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function MainContent() {
   return (
     <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+      <p>
+        I love to visit <span style={{ color: "red" }}>New York</span>,{" "}
+        <span style={{ color: "pink" }}>Paris</span>, and{" "}
+        <span style={{ color: "darkblue" }}>Tokyo</span>.
+      </p>
     </main>
   );
 }
