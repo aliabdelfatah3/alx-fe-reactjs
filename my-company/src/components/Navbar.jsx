6b1df90cd6bf7cf4d0ad33b3b43f510ca3b0ae04
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-18 20:38:06 +0300

    modified

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,28 +3,51 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#333", color: "#fff" }}>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <Link
         to="/"
-        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+        style={{
+          margin: "0 10px",
+          color: "#fff",
+          textDecoration: "none",
+        }}
       >
         Home
       </Link>
       <Link
         to="/about"
-        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+        style={{
+          margin: "0 10px",
+          color: "#fff",
+          textDecoration: "none",
+        }}
       >
         About
       </Link>
       <Link
         to="/services"
-        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+        style={{
+          margin: "0 10px",
+          color: "#fff",
+          textDecoration: "none",
+        }}
       >
         Services
       </Link>
       <Link
         to="/contact"
-        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+        style={{
+          margin: "0 10px",
+          color: "#fff",
+          textDecoration: "none",
+        }}
       >
         Contact
       </Link>
