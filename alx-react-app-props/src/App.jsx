80e306e1461b213086796c383cf0c28dfaf7fb90
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-18 19:59:01 +0300

    add

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import ProfilePage from "./components/ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 import "./App.css";
 
 function App() {
