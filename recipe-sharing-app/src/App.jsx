f202ea1dc97faca5a9d6198a64f348fbcc7cf7ca
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-25 22:04:37 +0300

    modified

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import AddRecipeForm from "./components/AddRecipeForm";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 import SearchBar from "./components/SearchBar";
 
 function App() {
@@ -13,17 +15,14 @@ function App() {
         <SearchBar />
         <Routes>
           <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/add-recipe" element={<AddRecipeForm />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
-const RecipeDetailsWrapper = () => {
-  const { id } = useParams();
-  return <RecipeDetails recipeId={id} />;
-};
 
 export default App;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,36 +2,22 @@ import create from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-
-  setRecipes: (recipes) => set({ recipes }),
-
-  addRecipe: (newRecipe) =>
+  favorites: [],
+  addFavorite: (recipeId) =>
+    set((state) => ({ favorites: [...state.favorites, recipeId] })),
+  removeFavorite: (recipeId) =>
     set((state) => ({
-      recipes: [...state.recipes, newRecipe],
+      favorites: state.favorites.filter((id) => id !== recipeId),
     })),
-
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-
-  deleteRecipe: (recipeId) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-    })),
-
-  searchTerm: "",
-  setSearchTerm: (term) => set({ searchTerm: term }),
-
-  filteredRecipes: [],
-  filterRecipes: () =>
-    set((state) => ({
-      filteredRecipes: state.recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-      ),
-    })),
+  recommendations: [],
+  generateRecommendations: () =>
+    set((state) => {
+      // Example recommendation logic
+      const recommended = state.recipes.filter(
+        (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
+      );
+      return { recommendations: recommended };
+    }),
 }));
 
 export { useRecipeStore };
