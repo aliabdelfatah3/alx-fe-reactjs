d235834ed7bdad7164fd0e3128068113b66a6e57
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-25 21:14:17 +0300

    modifed

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom";
 import { useRecipeStore } from "../store/recipeStore";
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      {filteredRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-          {/* Add a link to the detailed recipe page */}
-          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+          </div>
+        ))
+      ) : (
+        <p>No recipes available.</p>
+      )}
     </div>
   );
 };
