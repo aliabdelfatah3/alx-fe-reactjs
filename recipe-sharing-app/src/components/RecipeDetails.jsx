bcdba284222cba6fa6b8d678fd8f657cad4bf6bc
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-25 22:17:13 +0300

    modified

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,10 +1,12 @@
 import { useRecipeStore } from "./recipeStore";
+import { useParams } from "react-router-dom";
 import EditRecipeForm from "./EditRecipeForm";
 import DeleteRecipeButton from "./DeleteRecipeButton";
 
-const RecipeDetails = ({ recipeId }) => {
+const RecipeDetails = () => {
+  const { recipeId } = useParams();
   const recipe = useRecipeStore((state) =>
-    state.recipes.find((recipe) => recipe.id === recipeId)
+    state.recipes.find((recipe) => recipe.id === parseInt(recipeId))
   );
 
   if (!recipe) return <p>Recipe not found</p>;
@@ -14,7 +16,7 @@ const RecipeDetails = ({ recipeId }) => {
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
       <EditRecipeForm recipe={recipe} />
-      <DeleteRecipeButton recipeId={recipeId} />
+      <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
 };
