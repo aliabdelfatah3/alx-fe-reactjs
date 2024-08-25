ea2e11904d11c7d3f63cf926f3ec7f84381d2dc0
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-08-25 21:12:11 +0300

    modified

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,27 @@ import create from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+
   searchTerm: "",
   setSearchTerm: (term) => set({ searchTerm: term }),
+
   filteredRecipes: [],
   filterRecipes: () =>
     set((state) => ({
