d0d00fd4ab7a70a720cb59d1e49b8fabc850ff9c
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-09-01 17:15:49 +0300

    mod

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         />
 
         {/* Dynamic route for BlogPost */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
