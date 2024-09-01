a75b43b473401179e36671e4d394da8215bb5a4a
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-09-01 17:03:03 +0300

    mod

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -18,6 +18,9 @@ function PostsComponent() {
   return (
     <div>
       <h1>Posts</h1>
+
+      <button onClick={() => refetch()}>Refetch Posts</button>
+
       <ul>
         {data.map((post) => (
           <li key={post.id}>{post.title}</li>
