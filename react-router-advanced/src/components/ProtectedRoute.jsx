c43b55f01a37803218a3c7e4624ece8d2966bf81
Ali Abdelfattah <aliabdelfatah455@gmail.com>
2024-09-01 17:17:32 +0300

    add

---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ element, isAuthenticated }) {
+  return isAuthenticated ? element : <Navigate to="/" />;
+}
+
+export default ProtectedRoute;
