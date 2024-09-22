30fa55bdf22a7e01786c512dd57f086ccc984cae
3looloo <aliabdelfatah455@gmail.com>
2024-09-22 21:25:47 +0300

    mod

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,22 @@
-import axios from "axios"
-const githubApiKey = import.meta.env.VITE_GITHUB_API_KEY;
+// src/services/githubService.js
+import axios from "axios";
+
+const githubApiKey = import.meta.env.VITE_GITHUB_API_KEY; // Get the API key from environment variables
+
+// Function to fetch GitHub user data
 export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `token ${githubApiKey}`,
-    },
-  });
-  const data = await response.json();
-  return data;
+  try {
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`,
+      {
+        headers: {
+          Authorization: `token ${githubApiKey}`, // Include the API token in headers
+        },
+      }
+    );
+    return response.data; // Return the user data
+  } catch (error) {
+    console.error("Error fetching GitHub user data:", error);
+    throw error; // Re-throw the error for handling in the component
+  }
 };
